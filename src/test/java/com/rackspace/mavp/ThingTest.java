2eaa20fd472845d0d8485d5fefa977eebaa2cf91
Richard Sartor <richard.sartor@rackspace.com>
2015-12-02 11:04:16 -0600

    Add an actual test.

--- src/test/java/com/rackspace/mavp/ThingTest.java
+++ src/test/java/com/rackspace/mavp/ThingTest.java
@@ -1,4 +1,13 @@
 package com.rackspace.mavp;
 
-public class ThingTest {
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class ThingTest extends TestCase {
+
+    public void testThatNewObjectsAreNotNull() {
+        Thing t = new Thing();
+
+        Assert.assertNotNull(t);
+    }
 }
